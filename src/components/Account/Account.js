e0a3b4bfd9e870459b85a803c6fdb7c528bb6977
naveensomanna <naveensomanna26@gmail.com>
2018-06-06 15:13:43 +0530

    login form implemented

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import axios from 'axios';
+import {NavLink} from 'react-router-dom';
 import './account.css';
 
 export default class Account extends React.Component{
@@ -58,7 +59,7 @@ return(
 </div>
 <div className="dropdown_option_group dropdown_option_group_footer">
 <a href="" className="dropdown_option"><img  className="settings" src="https://s.imgur.com/desktop-assets/desktop-assets/icon-settings.7399c1dde71a66bb11f5674b02f8a5f1.svg" alt=""/><span>Settings</span></a>
-<a href="" className="dropdown_option"><img className="sign-out" src="https://s.imgur.com/desktop-assets/desktop-assets/icon-sign-out.21a5a43e3814a8148874cef50962eedf.svg" alt=""/><span>Sign Out</span></a>
+<a href="" className="dropdown_option"><img className="sign-out" src="https://s.imgur.com/desktop-assets/desktop-assets/icon-sign-out.21a5a43e3814a8148874cef50962eedf.svg" alt=""/><NavLink to="/"><span>Sign Out</span></NavLink></a>
 </div>
 <div className="top_arrow"></div>
 </div>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,49 @@
 import React from 'react';
-
-export default class Login extends React.Component{
-
+import'./login.css';
+import {NavLink,withRouter} from'react-router-dom';
+ class Login extends React.Component{
+   constructor(props){
+     super(props);
+     this.handleSubmit=this.handleSubmit.bind(this);
+   }
+handleSubmit=(e)=>{
+  e.preventDefault();
+ if( this.a.value==='naveen' && this.pass.value==='imgur'){
+  this.props.history.push('/home');
+}else{
+  alert("enter valid username and password");
+}
+}
 render(){
 return(
-<div className="login_main">
-<h1 style={{color:'white',textAlign:'center'}}>imgur</h1>
-<div style={{width:'400px',margin:'0 auto'}}>
-
-</div>
+<div className="login-screen">
+  <div className="left-item">
+    <div className="login-panel">
+      <div className="inner-login-panel">
+        <div className="content-panel">
+          <h3 className="title">Sign In</h3>
+          <form>
+            <input type="text" placeholder="username" ref={(text)=>{this.a=text}}/>
+            <input type="password" placeholder="password" ref={(pass)=>{this.pass=pass}}/>
+            <button type="button" value="Let's Go" onClick={this.handleSubmit}>Let's Go</button>
+          </form>
+        </div>
+      </div>
     </div>
+  </div>
+
+  <div class="right-item">
+    <div class="slider-panel">
+      <div class="inner-slider-panel">
+        <h1>Logo</h1>
+        <p>Sign in to acess your account.</p>
+      </div>
+    </div>
+  </div>
+</div>
 
 )
 }
 
 }
+export default withRouter(Login);
